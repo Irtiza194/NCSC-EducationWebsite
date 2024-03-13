70674a7d2918e9fc0dc58d8b0b14212b68e791d9
Irtiza194 <75379881+Irtiza194@users.noreply.github.com>
2024-03-13 14:39:33 +0000

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,33 @@
+const firstLink = document.getElementById("first");
+const secondLink = document.getElementById("second");
+const thirdLink = document.getElementById("third");
+const forthLink = document.getElementById("forth");
+const fifthLink = document.getElementById("fifth");
+const sixthLink = document.getElementById("sixth");
+const seventhLink = document.getElementById("seventh");
+const eigthLink = document.getElementById("eighth");
+
+firstLink.addEventListener("click", function () {
+  location.href = "https://www.cybrary.it/";
+});
+secondLink.addEventListener("click", function () {
+  location.href = "https://www.netacad.com/";
+});
+thirdLink.addEventListener("click", function () {
+  location.href = "https://www.offsec.com/";
+});
+forthLink.addEventListener("click", function () {
+  location.href = "https://www.coursera.org/learn/cyber-security-fundamentals";
+});
+fifthLink.addEventListener("click", function () {
+  location.href = "https://www.sans.org/";
+});
+sixthLink.addEventListener("click", function () {
+  location.href = "https://academy.b9lab.com/";
+});
+seventhLink.addEventListener("click", function () {
+  location.href = "https://owasp.org/";
+});
+eigthLink.addEventListener("click", function () {
+  location.href = "https://mobilesecuritywiki.com/";
+});
